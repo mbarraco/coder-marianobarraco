92ca7a91cee3bf3997fe1a878984dfd1dede60a1
Mariano Barraco <mariano@aidence.com>
2023-03-21 19:20:52 -0300

    push fix to dev 01

---
+++ clase_02.py
@@ -0,0 +1,9 @@
+# Ejemplo: calcular el promedio entre 6, 9 y 10 e imprimirlo por pantalla
+
+mi_primer_valor = 6
+mi_segundo_valor = 9
+mi_tercer_valor = 10
+
+mi_promedio = (mi_primer_valor + mi_segundo_valor + mi_tercer_valor) / 3
+
+print(mi_promedio)
