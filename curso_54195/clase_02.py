6ed9d094206c9fe7e9f10071834ec92182cbc723
Mariano Barraco <mariano@aidence.com>
2023-06-12 22:30:37 -0300

    clase 2

--- curso_54195/clase_02.py
+++ curso_54195/clase_02.py
@@ -29,7 +29,7 @@ pprint(mi_diccionario)
 
 
 ###############################################################################
-# Microdesafio 1
+# Ejemplo en vivo 1
 ###############################################################################
 from pprint import pprint
 
@@ -49,3 +49,75 @@ pprint(campeones)
 # campeones[2022] = "Argentina" # dos opciones
 campeones.update({2022: "Argentina"})
 pprint(campeones)
+
+###############################################################################
+# Ejemplo en vivo 2
+###############################################################################
+mi_conjunto = {"Rojo", "Blanco", "Azul"}
+print(mi_conjunto)
+mi_conjunto.add("Violeta")
+mi_conjunto.add("Dorado")
+mi_conjunto.remove("Dorado")
+mi_conjunto.remove("Blanco")
+mi_conjunto.discard("Celeste")
+print(mi_conjunto)
+# mi_conjunto.remove("Celeste")  No se puede hacer porque "Celeste" no pertenece al mi_conjunto
+
+###############################################################################
+# Ejemplo en vivo 3
+###############################################################################
+# texto = "gordon lanzó su curva&strawberry ha fallado por un pie! -gritó Joe Castiglione&dos pies -le corrigió Troop&strawberry menea la cabeza como disgustado… -agrega el comentarista"
+from pprint import pprint
+texto =  ("gordon lanzó su curva&strawberry ha fall"
+          "ado por un pie! -gritó Joe Castiglione&dos pie"
+          "s -le corrigió Troop&strawberry menea la cab"
+          "eza como disgustado… -agrega el comentarista")
+
+print("_" * 90)
+print(texto)
+print("_" * 90)
+
+lineas = texto.split("&")
+pprint(lineas)
+print("_" * 90)
+print("." * 90)
+
+print(lineas[0].capitalize() + "...")
+print("- " + lineas[1].capitalize() + ".")
+print("- " + lineas[2].capitalize() + ".")
+print("- " + lineas[3].capitalize() + ".")
+# print(f"- {lineas[3].capitalize()}.")
+print("." * 90)
+print("_" * 90)
+
+
+###############################################################################
+# Ejemplo en vivo 4
+###############################################################################
+xx = set([1,2,3])
+yy = set([3,4,5])
+zz = xx.difference(yy)
+print(zz)
+
+
+###############################################################################
+# Ejemplo en vivo 5
+###############################################################################
+mis_divisas = {'Dolar':'$','Euro':'€', 'Libra':'£'}
+divisa_solicitada = input("Ingresar divisa: ")
+simbolo = mis_divisas.get(divisa_solicitada.capitalize(), "Divisa no soportada")
+print(simbolo)
+# print(mis_divisas[divisa_solicitada]) # no sirve para monedas que no estén el diccionario!!
+
+###############################################################################
+# Ejemplo en vivo 6
+###############################################################################
+nombre = input("Ingresar nombre: ")
+edad = int(input("Ingresar edad: "))
+direccion = input("Ingresar direccion: ")
+datos_del_usuario = {
+    "nombre": nombre,
+    "edad": edad,
+    "direccion": direccion,
+}
+print(datos_del_usuario)
