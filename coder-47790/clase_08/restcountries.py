7be7b0ccc2edcf4e06df2110921ed3532dbbbe12
Mariano Barraco <mariano@datacoves.com>
2023-10-18 10:31:13 -0300

    clase 08

--- coder-47790/clase_08/restcountries.py
+++ coder-47790/clase_08/restcountries.py
@@ -1,9 +1,24 @@
 import requests
+import json
+
 
 direccion =  "http://restcountries.com/v3.1/all"
 
 respuesta = requests.get(direccion)
 
 print(respuesta.status_code)
-# print(respuesta.text)
-print(respuesta.json())
+print(type(respuesta.json()))
+
+paises_sin_procesar = respuesta.json()
+
+for i, pais_sin_procesar in enumerate(paises_sin_procesar):
+    # print(i, type(pais_sin_procesar))
+    # print(i, pais_sin_procesar.keys())
+    print(i, pais_sin_procesar["name"]["official"])
+
+
+
+mi_archivo = open("paises.json", "w")
+
+json.dump(paises_sin_procesar, mi_archivo)
+
