a8cfde3fea21b72272a490508bf795e592eb36d7
Mariano Barraco <mariano@datacoves.com>
2023-11-08 21:33:51 -0300

    after 01

---
+++ coder-47790/after_01/03_laberinto.py
@@ -0,0 +1,90 @@
+class Tablero:
+
+    altura = 8
+    base = 8
+
+    def __init__(self, entrada, salida):
+        self.entrada = entrada
+        self.salida = salida
+
+    def juego_finalizado(self, jugador):
+        if jugador.posicion == self.salida:
+            print("Juego finalizado: el jugador llegó a la salida")
+            return True
+        else:
+            print("Juego no finalizado")
+            return False
+
+
+    def imprimir(self, jugador):
+        x_jugador = jugador.posicion[0]
+        y_jugador = jugador.posicion[1]
+
+        x_salida = self.salida[0]
+        y_salida = self.salida[1]
+
+        for y in range(0, self.altura):
+            fila = ["_"] * self.base
+            if y == y_jugador:
+                fila[x_jugador] = "0"
+            if y == y_salida:
+                fila[x_salida] = "X"
+
+            # print(fila)
+            print("\t".join(fila))
+
+
+    def __str__(self):
+        return f"Tablero: <entrada> {self.entrada}, <salida> {self.salida}"
+
+
+class Jugador:
+
+    def __init__(self, nombre, posicion):
+        self.nombre = nombre
+        self.posicion = posicion
+
+    def caminar_arriba(self):
+        self.posicion[1] += 1
+
+    def caminar_abajo(self):
+        self.posicion[1] -= 1
+
+    def caminar_izquierda(self):
+        self.posicion[0] -= 1
+
+    def caminar_derecha(self):
+        self.posicion[0] += 1
+
+    def __str__(self):
+        return f"La posicion de {self.nombre.title()} es {self.posicion}"
+
+
+mi_tablero = Tablero([0,0], [5,4])
+mi_jugador = Jugador("Frodo", mi_tablero.entrada)
+
+
+movimientos_permitidos = ["w", "a", "s", "d"]
+while True:
+
+
+    mi_tablero.imprimir(mi_jugador)
+    if mi_tablero.juego_finalizado(mi_jugador):
+        break
+
+    movimiento = input(f"Hacia dónde debería moverse {mi_jugador.nombre}? {movimientos_permitidos} ")
+    print("_" * 90)
+    if movimiento not in movimientos_permitidos:
+        print("Movimiento no permitido")
+        continue
+
+    if movimiento == "w":
+        mi_jugador.caminar_arriba()
+    elif movimiento == "a":
+        mi_jugador.caminar_izquierda()
+    elif movimiento == "s":
+        mi_jugador.caminar_abajo()
+    else:
+        mi_jugador.caminar_derecha()
+
+
