35d17f5092645cc2d2725f33fb609ae2958c92a9
Mariano Barraco <marianobarraco@gmail.com>
2024-01-23 20:17:30 -0300

    clase 02

---
+++ curso_54130/clase-02/00_strings.py
@@ -0,0 +1,14 @@
+
+
+mi_texto = "Run you fools!"
+mi_otro_texto = "Por favor!"
+separador = "      "
+print()
+print(mi_texto)
+print()
+print(mi_texto[1])
+print()
+print(mi_texto[1:])
+print()
+print(mi_texto +  mi_otro_texto)
+print(mi_texto + separador + mi_otro_texto)
---
+++ curso_54130/clase-02/01_strings.py
@@ -0,0 +1,13 @@
+
+separador = "    "
+longitud = len(separador)
+print(separador)
+print(longitud)
+
+mi_texto = "abraCadabra"
+
+print(mi_texto)
+print(mi_texto[1:8])
+print(mi_texto[1:8:2])
+print(mi_texto[::-1])
+cadena_volteada = mi_texto[::-1]
